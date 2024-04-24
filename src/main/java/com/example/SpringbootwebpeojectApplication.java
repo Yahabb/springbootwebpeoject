26ccad05adff33664ad1bd57d613d646f2b872ac
Yahabb <2362227183@qq.com>
2024-04-24 13:34:59 +0800

    Initial commit

---
+++ src/main/java/com/example/SpringbootwebpeojectApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebpeojectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootwebpeojectApplication.class, args);
+    }
+
+}
